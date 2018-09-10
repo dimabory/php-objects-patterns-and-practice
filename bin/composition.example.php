37cd52c084f4619f008994a517939f1929305f36
Dmytro Borysovskyi <bbbara10@gmail.com>
2018-09-10 16:43:37 +0300

    [skip ci] import classes in composition.example

--- bin/composition.example.php
+++ bin/composition.example.php
@@ -5,15 +5,17 @@
 use Acme\Chapter8\Composition\{
     DailyRentStrategy,
     MonthlyRentStrategy,
-    Rent};
+    Rent,
+    RentABicycle,
+    RentACar};
 
 require_once 'autoload.php';
 
 try {
     $rents = [
-        'car 1'     => new \Acme\Chapter8\Composition\RentACar(100, 10, new DailyRentStrategy()),
-        'car 2'     => new \Acme\Chapter8\Composition\RentACar(100, 25, new MonthlyRentStrategy()),
-        'bicycle 2' => new \Acme\Chapter8\Composition\RentABicycle(10, 2, new MonthlyRentStrategy()),
+        'car 1'     => new RentACar(100, 10, new DailyRentStrategy()),
+        'car 2'     => new RentACar(100, 25, new MonthlyRentStrategy()),
+        'bicycle 2' => new RentABicycle(10, 2, new MonthlyRentStrategy()),
     ];
 
 
